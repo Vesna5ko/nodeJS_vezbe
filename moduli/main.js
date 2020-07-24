82ed3cbca4a573e95d0488b38d59e7d227d2e2ad
Vesna Petkovic <vesna.5kovic@gmail.com>
2020-07-24 17:58:48 +0200

    Moduli

---
+++ moduli/main.js
@@ -0,0 +1,7 @@
+let rendomize = require('./second');
+
+let numbers = [1,2,3,4,5]; 
+
+let randNumbers = rendomize(numbers);
+
+console.log(randNumbers);
---
+++ moduli/second.js
@@ -0,0 +1,11 @@
+const rendomize = function(arr){
+    let copyArr = [].concat(arr);
+    let arrForExport = [];
+    for(let i=0; i < arr.length; i++){
+        let rand = Math.floor(Math.random() * copyArr.length);
+        arrForExport.push(copyArr[rand]);
+        copyArr.splice(rand,1);
+    }
+    return arrForExport;
+}
+module.exports = rendomize;
